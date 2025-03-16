8a185f1b7b523200469bf03efa7c2674952be621
lazareskoczur171 <lazareskoczur171@gmail.com>
2025-03-16 19:37:17 +0000

    Add 10.java

---
+++ src/10.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+
+public class GitHubSchool {
+    public static void main(String[] args) {
+        Random r = new Random();
+        int x = r.nextInt(10);
+        System.out.println("Random number between 0 and 9: " + x);
+    }
+}
