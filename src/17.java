1284dfb214ec6963d67a0981e47354b1254326fc
lazareskoczur171 <lazareskoczur171@gmail.com>
2025-03-24 02:46:15 +0000

    Add 17.java

---
+++ src/17.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class SimpleGithubSchoolProject {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello World!");
+        scanner.close();
+    }
+}
