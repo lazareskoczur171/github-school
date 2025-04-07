9a30728aacecb1fe0d0ddf3a872df06e136a79ca
lazareskoczur171 <lazareskoczur171@gmail.com>
2025-04-07 05:10:54 +0000

    Add 33.java

---
+++ src/33.java
@@ -0,0 +1,16 @@
+public class SimpleJavaCode {
+    public static void main(String[] args) {
+        // Example of using a loop to iterate through numbers
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Iteration " + i);
+        }
+
+        // Another example with a variable for an integer
+        int num = 5;
+        if (num > 0) {
+            System.out.println(num + " is positive.");
+        } else if (num == 0) {
+            System.out.println(num + " is zero.");
+        }
+    }
+}
