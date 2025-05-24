f1b0a74cef52fce3dbd5f67ef0858ffaa2f8426c
lazareskoczur171 <lazareskoczur171@gmail.com>
2025-05-24 10:05:05 +0000

    Add 52.java

---
+++ src/52.java
@@ -0,0 +1,6 @@
+public class GitHubSchoolApp {
+    public static void main(String[] args) {
+        // Example usage of a simple Java program
+        System.out.println("Hello, World!");
+    }
+}
